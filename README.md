098449b6243f1a81e51ceebb5a17437d43298d20
Daniel Esparza <15037512+d-Esparza19@users.noreply.github.com>
2020-09-23 23:58:18 -0500

    Initial Commit + DB setup + app setup + index

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # blogProject
- A personal blog site made with guidance from the web developer bootcamp.
+ A personal blog site to practice RESTful routing made with guidance from the web developer bootcamp using Semantic UI.
---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+    //App Config
+    mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser: true, useUnifiedTopology: true});
+    
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static("public"));
+    app.set('view enginge', 'ejs');
+   
+
+
+    var blogSchema = new mongoose.Schema({
+        title: String,
+        image: {type: String, default: "https://flic.kr/p/cRB6Wj"},
+        body: String,
+        created: {type: Date, default: Date.now}
+    });
+
+    //Mongoose/Model config
+    var Blog = mongoose.model("Blog",blogSchema);
+
+    app.get("/",(req,res)=>{
+        res.redirect("/blogs");
+    });
+
+    app.get("/blogs", (req,res)=>{
+        Blog.find({}, (err,blogs)=>{
+            if(err){
+                console.log("error");
+            }else{
+                res.render("index.ejs",{blogs: blogs});
+            }
+        }
+            )
+        
+    });
+
+
+//Routes
+    const port = 3000
+
+    app.listen(port, ()=>{
+        console.log("Server Running");
+    });
+
